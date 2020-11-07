c6ea07888b0ed0ab7ca684433014bb8eec50c322
elisa-adacado <echan@adacado.com>
2020-11-07 10:56:37 -0800

    added cache; some formatting

--- src/main.go
+++ src/main.go
@@ -9,13 +9,17 @@ import (
 
 	"github.com/julienschmidt/httprouter"
 )
+
 type server struct {
 	r *httprouter.Router
 }
 
 var (
-	openWeatherAPIKEY = os.Getenv("OPEN_WEATHER_API_KEY")
+	openWeatherAPIKEY    = os.Getenv("OPEN_WEATHER_API_KEY")
 	openWeatherMapDomain = "api.openweathermap.org"
+	//cache will hold city and weather info for 5 min (assumes weather does not change much in that time)
+	//used to prevent unnecessary repeat/duplicate calls to api
+	miniWeatherCache = make(map[string][]byte)
 )
 
 func main() {
@@ -27,31 +31,38 @@ func main() {
 }
 
 
-
+// returns weather info based off city in query string
+// if city was called within 5 min prior, will return last set info; 
+// otherwise will fetch new weather data from api and load cache
 func getWeatherByCity(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	keys, ok := r.URL.Query()["city"]
-    if !ok || len(keys[0]) < 1 {
-        log.Printf("ERROR: getWeatherByCity(): param city not provided")
-        return
-	}	
+	if !ok || len(keys[0]) < 1 {
+		log.Printf("ERROR: getWeatherByCity(): param city not provided")
+		return
+	}
 	city := keys[0]
 	log.Printf("city: %s", city)
-	url := fmt.Sprintf("https://%s/data/2.5/weather?q=%s&appid=%s",openWeatherMapDomain, city, openWeatherAPIKEY)
-	resp, err := http.Get(url)
-	if err != nil {
-		log.Printf("ERROR: getWeatherByCity() error fetching weather for url %s : %v", url, err)
-		return
+	if val, ok := miniWeatherCache[city]; ok {
+		log.Printf("INFO: getWeatherByCity(): city %s previously fetched, returning last given info", city)
+		w.Write(val)
+	} else {
+		url := fmt.Sprintf("https://%s/data/2.5/weather?q=%s&appid=%s", openWeatherMapDomain, city, openWeatherAPIKEY)
+		resp, err := http.Get(url)
+		if err != nil {
+			log.Printf("ERROR: getWeatherByCity() error fetching weather for url %s : %v", url, err)
+			return
+		}
+		defer resp.Body.Close()
+		weather, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Printf("ERROR: getWeatherByCity() error reading from for url %s", url)
+			return
+		}
+		// add to cache
+		miniWeatherCache[city] = weather
+		w.Write(weather)
 	}
-	defer resp.Body.Close()
-	weather, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Printf("ERROR: getWeatherByCity() error reading from for url %s", url)
-		return
-	}
-	w.Write(weather)
-	return 
 }
-
 
 
 
